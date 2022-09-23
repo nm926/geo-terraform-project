305aab8145fe424b4a72472f69cb388ab5634a47
Nilesh Mishra <nileshmishra@yandex.com>
2022-09-23 18:10:08 +0530

    ID attribute add in route

--- modules/VPC/maint.tf
+++ modules/VPC/maint.tf
@@ -47,7 +47,7 @@ resource "aws_subnet" "geo-subnet-public2" {
 
 #aws_internet_gateway
 resource "aws_internet_gateway" "geo-igw" {
-vpc_id = var.vpc-geo
+vpc_id = "var.vpc-geo.id"
 
   tags = {
     Name = "${var.env_prefix}-igw"
@@ -56,7 +56,7 @@ vpc_id = var.vpc-geo
 
 
 resource "aws_default_route_table" "d-route-1" {
-  default_route_table_id =  var.vpc-geo
+  default_route_table_id =  "var.vpc-geo.id"
 
   route {
     cidr_block = "0.0.0.0/0" 
