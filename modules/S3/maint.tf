bf627960c253abedad6b572bf02dd8e4e37d9e7c
Nilesh Mishra <nileshmishra@yandex.com>
2022-09-23 21:34:56 +0530

    enhance in s3 bucket policy with secure key

--- modules/S3/maint.tf
+++ modules/S3/maint.tf
@@ -11,20 +11,31 @@ resource "aws_kms_key" "geo-key" {
 }
 
 
-resource  "aws_s3_bucket" "geo-bucket" {
-  tags = {
-    Name = "s3 ${random_id.server.hex}"
-  }
-
+resource "aws_s3_bucket" "bucket-kms-key" {
   bucket = random_id.server.hex
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.geo-key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
   }
+}
+
+
+resource "aws_s3_bucket_acl" "geo-acl" {
+  bucket = random_id.server.hex
+  acl    = "private"
+}
 
 
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = random_id.server.hex
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
 }
+
 
 data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
@@ -40,3 +51,5 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
 
   }
 }
+
+
